0140f7117fb588267531a2c78f56faedbc624d4b
Dongjin Na <dongjinna@gmail.com>
2018-05-02 01:37:09 +0900

    Implement findAll function (#7)
    Implement findAll function.

--- generator/idl_finder.js
+++ generator/idl_finder.js
@@ -1,8 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+async function readDirectory(directory) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directory, (error, files) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(files.map((file) => path.resolve(directory, file)));
+      }
+    })
+  });
+}
+
+async function isDirectory(path) {
+  return new Promise((resolve, reject) => {
+    fs.lstat(path, (error, stat) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stat.isDirectory());
+      }
+    });
+  });
+}
+
+// TODO(nadongguri): Should not use recursive.
+async function findNidl(directory) {
+  const nidlList = [];
+  const files = await readDirectory(directory);
+  for (const file of files) {
+    if (await isDirectory(file) &&
+        path.basename(file) !== 'node_modules') {
+      nidlList.push(...await findNidl(file)); 
+    } else if (path.extname(file) === '.nidl') {
+      nidlList.push(file);
+    }
+  }
+  return nidlList;
+}
+
 async function findAll() {
-  // TODO(zino): Should implement this function.
-  return [
-    `${__dirname}/../examples/hello.nidl`
-  ];
+  return findNidl(process.cwd());
 }
 
 module.exports = {
